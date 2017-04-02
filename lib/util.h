2beb66bce1bda764e16f958b6012d6a294c1881b
Jayasheela Prakasha <jayasheela.prakasha@gmail.com>
2017-04-02 23:18:46 +0530

    spell check

--- lib/util.h
+++ lib/util.h
@@ -1,4 +1,4 @@
-/*******************************************************************************
+/**
  *
  *  EE450 Socket Programming Project 
  *      -A university project
@@ -7,12 +7,12 @@
  *
  *  No License; Any body are free to use, edit, and distribute.
  *   
- ******************************************************************************/
+ */
 
 #ifndef _UTIL_H
 #define _UTIL_H 1
 
-/*
+/**
  * Includes
  */
 
@@ -20,18 +20,14 @@
 #include <stdint.h>
 #include <string.h>
 
-/*
+/**
  * Macro Definitions
  */
 
 /* Maximum 10 bits to represent a number + 1 null */
 #define BITS 11 
 
-#define LOG_ERROR(s, ...) fprintf(strerr, "ERROR occurred at Line number %d"\
-                                        "in %s\n%s ", __LINE__, __FILE__, s,\
-                                        ##__VA_ARGS__)
-                                        
-/*
+/**
  *  Function Decelerations
  */
 
