732797aad56e0f6e8d2582a0a908536ce23dbd9e
Jayasheela Prakasha <jayasheela.prakasha@gmail.com>
2017-04-02 23:22:45 +0530

    Spell check

--- src/common.h
+++ src/common.h
@@ -1,4 +1,4 @@
-/*******************************************************************************
+/**
  *
  *  EE450 Socket Programming Project 
  *      -A university project
@@ -7,15 +7,16 @@
  *
  *  No License; Any body are free to use, edit, and distribute.
  *   
- ******************************************************************************/
+ */
  
  
  #ifndef _COMMON_H
  #define _COMMON_H 1
  
- /*
+/**
  * Includes
  */
+ 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -29,18 +30,31 @@
 #include <sys/wait.h>
 #include <stdint.h>
 
-/*
+/**
  * Macro Definitions
  */
 
-/* Maximum 10 bits to represent a number + 1 null */
+/* Maximum 10 bits to represent a number + 1 for null */
 #define BITS 11 
 
-#define LOG_ERROR(s, ...) fprintf(strerr, "ERROR occurred at Line number %d"\
-                                        "in %s\n%s ", __LINE__, __FILE__, s,\
-                                        ##__VA_ARGS__)
+/* Maximum 100 line of data may be present in job.txt file */
+#define MAX_LINES 100
 
-/*
+/* Maximum size of data sent over the socket */
+#define MAX_DATA ( MAX_LINE * sizeof(struct line_data) )
+
+/* Error logs*/
+#define UNKNOWN_ERROR(s, ...) fprintf(stderr, "ERROR occurred at Line number:"\
+                                              " %d in %s\n%s ", __LINE__,\
+                                               __FILE__, (s, ##__VA_ARGS__))
+                                        
+#define KNOWN_ERROR(s) fprintf(stderr, "ERROR occurred at Line number %d "\
+                                        "in %s\n%s : %s\n ", __LINE__, __FILE__\
+                                        , s, strerror(errno))
+
+
+
+/**
  * Enumeration and structure definitions
  */
  
@@ -54,27 +68,32 @@ enum bools {
 	true,
 };
 
-/*
+/**
  *  struct line_data: A structure which the information present in each line,
- *                     read from the file. Serialised structer on a buffer 
+ *                     read from the file. Serialised structure on a buffer 
  *                     travel over a socket
  *  Structure Members:
- *  @idx        : Index of the line a.k.a line number starts from 0 (Size:7 bits, Maximum value be 99).
- *  @operation  : Operation to be performed bitwise-OR or bitwise-AND ().
- *  @operand1   : First operand( Size:10bits, Maximun 10 bits given in specification).
- *  @operand2   : Second operand( Size:10bits, Maximun 10 bits given in specification).
- *  @result     : Result( Size:10bits, Maximun 10 bits given in specification).
+ *  @idx        : Index of the line a.k.a line number starts from 0 (Size:7 bits, Maximum value may be 99).
+ *  @operator	: Operation to be performed bitwise-OR or bitwise-AND (Size: 1 bit, 0:OR 1:AND).
+ *  @operand1   : First operand( Size:10 bits, Maximum 10 bits given in specification).
+ *  @operand2   : Second operand( Size:10 bits, Maximum 10 bits given in specification).
+ *  @result     : Result( Size:10 bits, Maximum 10 bits given in specification).
  *  @end        : Indicates no more data present ahead
  */
 struct line_data {
     uint64_t idx        : 7;
-    uint64_t opertion   : 1;
+    uint64_t operator   : 1;
     uint64_t operand1   : 10;
     uint64_t operand2   : 10;
     uint64_t            : 4;
     uint64_t result     : 10;
     uint64_t end        : 1;
     uint64_t            : 21;
+};
+
+struct buf_over_socket {
+	size_t size;
+	char data[ MAX_DATA];
 };
 
  #endif /* _COMMON_H */
